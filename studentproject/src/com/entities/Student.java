d13135f45d8e708a626d11f01fd1180ff33ebf25
jai84322 <jai84322@gmail.com>
2023-02-16 11:42:49 +0530

    first commit

---
+++ studentproject/src/com/entities/Student.java
@@ -0,0 +1,57 @@
+package com.entities;
+
+	public class Student implements Comparable<Student> {
+		
+	 	private String name;
+	    private int age;
+	    private String email;
+	    private int rollNo;
+
+	    public Student(String name, int age, String email, int rollNo) {
+	        this.name = name;
+	        this.age = age;
+	        this.email = email;
+	        this.rollNo = rollNo;
+	    };
+
+	    public void setName(String name) {
+	        this.name = name;
+	    };
+
+	    public String getName() {
+	        return name;
+	    };
+
+	    public void setAge(int age) {
+	        this.age = age;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public int getRollNo() {
+	    	return rollNo;
+	    }
+	    
+	    public void setRollNo(int num) {
+	    	this.rollNo = num;
+	    }
+
+		@Override
+		public int compareTo(Student o) {
+			return this.age - o.age;
+		}
+
+}
+
+
+
